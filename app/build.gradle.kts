a92cafdcb54075886a594d0d5570215c55ac78bb
ddandres <ddandres@disca.upv.es>
2026-01-20 17:58:47 +0100

    Updated to AGP 9.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,6 @@
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -42,12 +41,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlin {
-        compilerOptions {
-            languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_2
-            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-        }
     }
     buildFeatures {
         viewBinding = true
